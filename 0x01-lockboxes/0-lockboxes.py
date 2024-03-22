a3895e85ee90a611591baa1701afd407eb7ee09c
RogersKaran <karanjarodgers@gmai.com>
2024-03-22 12:37:33 +0000

    updates

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -26,13 +26,4 @@ def canUnlockAll(boxes):
     
     return len(visited) == n
 
-if __name__ == "__main__":
-    # Test cases
-    boxes1 = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes1))  # Output: True
 
-    boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes2))  # Output: True
-
-    boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes3))  # Output: False

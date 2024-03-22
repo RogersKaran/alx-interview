182a57944a678c7d47f985fe9b0bdb05d88a0964
RogersKaran <122857486+RogersKaran@users.noreply.github.com>
2024-03-22 23:56:17 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
+
 """
 Calculates the minimum number of operations needed to achieve
 exactly n H characters in a text file using only "Copy All" and "Paste" operations.
 """
 
 def minOperations(n):
-    if n <= 1:
-        return 0
-    
-    # Initialize the number of operations needed for each index
-    operations = [0] * (n + 1)
-    
-    for i in range(2, n + 1):
-        # Initially, set the number of operations for each index to be equal to the index
-        operations[i] = i
-        
-        # Check if the current index (i) is a factor of n
-        for j in range(2 * i, n + 1, i):
-            # Update the number of operations needed for index j
-            operations[j] = min(operations[j], operations[i] + (j // i))
+    """
+        args: n: Number of characterfs to be displayed
+        return: number of min operations
+    """
 
-    # The minimum number of operations to achieve n H's is stored at index n
-    return operations[n]
-
+    now = 1
+    start = 0
+    counter = 0
+    while now < n:
+        reminder = n - now
+        if (reminder % now == 0):
+            start = now
+            now += start
+            counter += 2
+        else:
+            now += start
+            counter += 1
+    return counter

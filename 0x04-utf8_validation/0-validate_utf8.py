b200c0f875fed4f131f4b5779eb4f94f4dca2b58
RogersKaran <122857486+RogersKaran@users.noreply.github.com>
2024-04-03 18:14:36 +0300

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,20 +1,39 @@
-def validUTF8(data):
-    num_bytes = 0
-    for byte in data:
-        if num_bytes == 0:
-            if byte >> 7 == 0:
-                continue  # Single byte character
-            elif byte >> 5 == 0b110:
-                num_bytes = 1  # Two-byte character
-            elif byte >> 4 == 0b1110:
-                num_bytes = 2  # Three-byte character
-            elif byte >> 3 == 0b11110:
-                num_bytes = 3  # Four-byte character
-            else:
-                return False  # Invalid UTF-8 encoding
-        else:
-            if byte >> 6 != 0b10:
-                return False  # Invalid UTF-8 encoding
-            num_bytes -= 1
-    return num_bytes == 0  # True if all characters have been validated
+#!/usr/bin/python3
+"""UTF-8 Validation"""
 
+
+def validUTF8(data):
+    """Determine a given data set
+    reps a valid UTF-8 encoding
+    """
+    number_bytes = 0
+
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
